bae8d7782089ba6ec778c65a0b7724c1c5ea334c
Alnino18 <73096929+Alnino18@users.noreply.github.com>
2026-05-14 00:30:50 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,10 +15,19 @@ self.addEventListener("install", event => {
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("install", evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", evt => {
+  evt.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
   );
 });
